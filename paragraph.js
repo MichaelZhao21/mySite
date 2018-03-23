0a78b6fb6e298cc8dc5f0c1d2538e082a18506b6
dragocat <michaelzhao314@gmail.com>
2018-03-23 11:30:19 -0500

    Initial upload

---
+++ paragraph.js
@@ -0,0 +1,7 @@
+function confirm(choice){
+	switch(choice){
+		case(0):
+			document.getElementById("source").innerHTML = "EWWWWWWWWWWWWWWWWWw";
+		case(1):
+			document.getElementById("source").innerHTML = "YAAAAAAAAAAAAAASSSS";
+}
