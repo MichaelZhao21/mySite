e36f23561800a01adfe9f734f4269874aaf14adc
dragocat <michaelzhao314@gmail.com>
2018-03-23 20:13:49 -0500

    Add game framework

--- canvasDraw.js
+++ canvasDraw.js
@@ -1,5 +1,12 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
-ctx.stroke();
+function line(){
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	ctx.moveTo(0, 0);
+	ctx.lineTo(200, 100);
+	ctx.stroke();
+}
+function clearCanvas(){
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	ctx.clearRect(0, 0, 200, 100);
+}
