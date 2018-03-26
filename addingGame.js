64835b9fe0fb17a6c5fbb3799a861b9ab1bc76b6
dragocat <michaelzhao314@gmail.com>
2018-03-26 10:46:36 -0500

    Added enter button, removed check clicky

--- addingGame.js
+++ addingGame.js
@@ -8,8 +8,10 @@ function check(){
 	}else{
 		document.getElementById("correct").innerHTML = x.value + " is wrong, " + correct + " is the right answer.";
 		window.setTimeout(function(){document.getElementById("correct").innerHTML = "";}, 2000);
-	}	
+	}
 	newQuestion();
+	//ghghghghghghhgghgh
+	document.getElementById("answer").value = "";
 }
 function newQuestion(){
 	var da = document.getElementById("d1").value;
@@ -18,7 +20,7 @@ function newQuestion(){
 	var b = randomInt(Math.pow(10,db-1),Math.pow(10,db));
 	correct = a + b;
 	document.getElementById("question").innerHTML = String(a) + "+" + String(b);
-	
+
 }
 function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
@@ -28,3 +30,12 @@ function start(){
 	var da = document.getElementById("d2").value = 1;
 	newQuestion();
 }
+function key(){
+	y = document.getElementById("answer");
+	y.addEventListener("keydown", function onEvent(event) {
+		if (event.key == "Enter"){
+			check();
+		}
+	});
+}
+key();
