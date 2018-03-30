e74b08a8c3411e58d79c7fc50e508852541e74d6
dragocat <michaelzhao314@gmail.com>
2018-03-30 08:09:04 -0500

    Sudoku can now generate a completed grid, still WIP

--- sudokuGen.js
+++ sudokuGen.js
@@ -6,13 +6,26 @@ var grid = [[],[],[],[],[],[],[],[],[]];
       grid[x][y] = 0;
     }
   }
-
   var tempGrid = possibleNumbers;
+  var shiftGrid3 = [3,4,5,6,7,8,0,1,2]
+  var shiftGrid1 = [1,2,3,4,5,6,7,8,0]
   var spot;
-  for(var x = 0; x < 9; x++){
+  for(var y = 0; y < 9; y++){
     spot = randomInt(0,tempGrid.length);
-    grid[0][x] = tempGrid[spot];
+    grid[0][y] = tempGrid[spot];
     tempGrid.splice(spot,1);
+  }
+  for (var y = 1; y < 9; y++){
+    if (y%3 == 0){
+      for (var x = 0; x < 9; x++){
+        grid[y][x] = grid[y-1][shiftGrid1[x]];
+      }
+    }
+    else{
+      for (var x = 0; x < 9; x++){
+        grid[y][x] = grid[y-1][shiftGrid3[x]];
+      }
+    }
   }
   drawTable(grid);
 }
