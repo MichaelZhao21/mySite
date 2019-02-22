28895cefa275f6d5cff8a3c8f464835cb6ae35ae
dragocat <michaelzhao314@gmail.com>
2019-02-22 10:11:44 -0600

    Refactored part of the sudoku gen

--- subpages/sudokuGen/sudokuGen.js
+++ subpages/sudokuGen/sudokuGen.js
@@ -1,52 +1,73 @@
-var grid = [[],[],[],[],[],[],[],[],[]];
-var correctGrid = [[],[],[],[],[],[],[],[],[]];
+var gameGrid = [];
+var solutionsGrid = [];
+const POSSIBLE_NUMBERS = [1,2,3,4,5,6,7,8,9];
+var tempGrid = POSSIBLE_NUMBERS;
+const EMPTY_SPACE = 0;
+const CELLS_PER_ROW = 9;
 
-function makeAnswer(){
-var possibleNumbers = [1,2,3,4,5,6,7,8,9];
-  for (var x = 0; x < 9; x++){
-    for (var y = 0; y < 9; y++){
-      grid[x][y] = 0;
-    }
-  }
-  var tempGrid = possibleNumbers;
-  var shiftGrid3 = [3,4,5,6,7,8,0,1,2];
-  var shiftGrid1 = [1,2,3,4,5,6,7,8,0];
+function initializeGame() {
+  createSolutionsGrid();
+  drawTable();
+  setNumberOfCellsToEmpty(20);
+}
+
+function createSolutionsGrid(){
+  clearGameGrid();
   var spot;
-  for(var y = 0; y < 9; y++){
-    spot = randomInt(0,tempGrid.length);
-    grid[0][y] = tempGrid[spot];
-    tempGrid.splice(spot,1);
-  }
-  for (var y = 1; y < 9; y++){
+  setFirstCellInEachRow();
+  for (var y = 1; y < CELLS_PER_ROW; y++){
     if (y%3 == 0){
       for (var x = 0; x < 9; x++){
-        grid[y][x] = grid[y-1][shiftGrid1[x]];
+        gameGrid[y][x] = gameGrid[y-1][shiftGridIndicesByNumOfSpaces(x, 3)];
       }
     }
     else{
       for (var x = 0; x < 9; x++){
-        grid[y][x] = grid[y-1][shiftGrid3[x]];
+        gameGrid[y][x] = gameGrid[y-1][shiftGridIndicesByNumOfSpaces(x, 1)];
       }
     }
   }
+  var solutionsGrid = JSON.parse(JSON.stringify(gameGrid));
+  console.log(solutionsGrid);
+}
+
+function clearGameGrid() {
+  var tempSingleLine = [];
+  gameGrid = [];
   for (var x = 0; x < 9; x++){
     for (var y = 0; y < 9; y++){
-      correctGrid[x][y] = grid[x][y];
+      tempSingleLine.push(EMPTY_SPACE);
     }
+    gameGrid.push(tempSingleLine);
+    tempSingleLine = [];
   }
-  drawTable();
-  makeSpotsEmpty(20);
+}
+
+function shiftGridIndicesByNumOfSpaces(index, spaces) {
+  newIndex = index + spaces;
+  if (newIndex >= 9) {
+    newIndex %= 9;
+  }
+  return newIndex;
+}
+
+function setFirstCellInEachRow() {
+  for(var y = 0; y < CELLS_PER_ROW; y++){
+    spot = randomInt(0,tempGrid.length);
+    gameGrid[0][y] = tempGrid[spot];
+    tempGrid.splice(spot,1);
+  }
 }
 
 function drawTable(){
   for (x = 0; x < 9; x++){
     for (y = 0; y < 9; y++){
       z = document.getElementById("box" + String(x) + String(y));
-      if (grid[x][y] == 0){
+      if (gameGrid[x][y] == 0){
         z.innerHTML = "<input type=\"text\" id=\"answer" + String(x) + String(y) + "\" maxlength=\"1\"></input>";
       }
       else{
-        z.innerHTML = grid[x][y];
+        z.innerHTML = gameGrid[x][y];
       }
     }
   }
@@ -56,36 +77,36 @@ function findEmptySpots(){
   var emptyGrid = [];
   for (var x = 0; x < 9; x++){
     for (var y = 0; y < 9; y++){
-      if(grid[x][y] == 0){
+      if(gameGrid[x][y] == 0){
         emptyGrid.append(String(x) + String(y));
       }
     }
   }
 }
 
-function makeSpotsEmpty(spots){
+function setNumberOfCellsToEmpty(cells){
   var r;
   var c;
   var saveNum;
   var giveUp = 0;
   var test = 0;
-  for (var x = 0; x < spots; x++){
+  for (var x = 0; x < cells; x++){
     test = 0;
     giveUp = 0;
     while (test == 0){
       r = randomInt(0,9);
       c = randomInt(0,9);
-      if (grid[r][c] == 0){
+      if (gameGrid[r][c] == 0){
         test = 0;
       }
       else{
-        saveNum = grid[r][c];
-        grid[r][c] = 0;
+        saveNum = gameGrid[r][c];
+        gameGrid[r][c] = 0;
         if (giveUp > 100){
           test = 1;
         }
         if (isPossible() == false){
-          grid[r][c] = saveNum;
+          gameGrid[r][c] = saveNum;
           console.log(String(r) + String(c));
           giveUp++;
         }
@@ -103,13 +124,13 @@ function testCorrect(){
   var allRight = 0;
   for (var x = 0; x < 9; x++){
     for (var y = 0; y < 9; y++){
-      if (grid[x][y] == 0){
+      if (gameGrid[x][y] == 0){
         value = document.getElementById("answer" + String(x) + String(y)).value;
         if (value == ""){
           document.getElementById("answer" + String(x) + String(y)).style.color = "";
           allRight++;
         }
-        else if (Number(value) != correctGrid[x][y]){
+        else if (Number(value) != solutionsGrid[x][y]){
           document.getElementById("answer" + String(x) + String(y)).style.color = "red";
           allRight++;
         }
@@ -128,7 +149,7 @@ function isPossible(){
   var imagGrid = [[],[],[],[],[],[],[],[],[]];
   for (var x = 0; x < 9; x++){
     for (var y = 0; y < 9; y++){
-      imagGrid[x][y] = grid[x][y];
+      imagGrid[x][y] = gameGrid[x][y];
     }
   }
   var num;
@@ -145,7 +166,7 @@ function isPossible(){
     for (var l = 0; l <zeros.length; l++){
       x = String(zeros[l]).substr(0,1);
       y = String(zeros[l]).substr(1,1);
-      if (grid[x][y] == 0){
+      if (gameGrid[x][y] == 0){
         ans = getAnswer(x, y);
         if (ans != 0){
           zeros.splice(l,1);
@@ -167,7 +188,7 @@ function getAnswer(row, column){
   var testRow;
   var testBox;
   for (var x = 0; x < 9; x++){
-    testRow = testInArray(testNum, grid[row][x]);
+    testRow = testInArray(testNum, gameGrid[row][x]);
     if (testRow != -1){
       testNum.splice(testRow,1);
     }
@@ -176,7 +197,7 @@ function getAnswer(row, column){
     return testNum[0];
   }
   for (var x = 0; x < 9; x++){
-    testColumn = testInArray(testNum, grid[x][column]);
+    testColumn = testInArray(testNum, gameGrid[x][column]);
     if (testColumn != -1){
       testNum.splice(testColumn,1);
     }
@@ -193,7 +214,7 @@ function getAnswer(row, column){
     testzx = testGrid[x];
     zx = testzx.substr(0,1);
     zy = testzx.substr(1,1);
-    testBox = testInArray(testNum, grid[zx][zy]);
+    testBox = testInArray(testNum, gameGrid[zx][zy]);
     if (testBox != -1){
       testNum.splice(testBox,1);
     }
@@ -223,7 +244,7 @@ function testForZeros(){
   var zeroList = [];
   for (var x = 0; x < 9; x++){
     for (var y = 0; y < 9; y++){
-      if (grid[x][y] == 0){
+      if (gameGrid[x][y] == 0){
         zeroList.push(String(x) + String(y));
       }
     }
