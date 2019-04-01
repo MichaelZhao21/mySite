8fbf769f75e072cbc312f503770e11285905d820
MichaelZhao21 <michaelzhao314@gmail.com>
2019-04-01 13:16:56 -0500

    Added user input

--- subpages/hangman/hangman.js
+++ subpages/hangman/hangman.js
@@ -37,16 +37,29 @@ var drawPhases = [
 ];
 
 var hangmanPhase;
+var ansArray = [];
 
 function setup() {
   $(".hangmanDrawing").html(arrToHTML(drawPhases[0]));
-  hangmanPhase = -1;
-  $(".hangmanDrawing").css("display", "none");
+  hangmanPhase = 0;
+  $(".guess").css("display", "none");
+  $(":input[name='submitAnswer']").click(function (event) {
+    startGame();
+  });
+  $(":input[name='randomAnswer']")
 }
 
-$(document).keydown(function (event) {
-  incrementAndDrawHangman();
-});
+function startGame() {
+    var correctAnswerString = $(":input[name='inputAnswer']").val();
+    for (var x = 0; x < correctAnswerString.length; x++) {
+      ansArray.push(correctAnswerString.substring(x, x + 1));
+    }
+    console.log(ansArray);
+}
+
+// $(document).keydown(function (event) {
+//   incrementAndDrawHangman();
+// });
 
 function incrementAndDrawHangman() {
   hangmanPhase++;
